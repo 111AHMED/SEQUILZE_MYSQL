d03149d652f34516496cd647d939a392d62f8f06
111AHMED <ahmadosak@gmail.com>
2022-07-11 18:28:04 +0100

    migrationv4

--- models/roles.js
+++ models/roles.js
@@ -3,7 +3,7 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Roles extends Model {
     /**
-     * Helper method for defining associations.
+     * Helper method  for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
